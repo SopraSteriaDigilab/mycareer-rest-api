83b47c4e8fe48831d275d23361b5eb28ff36aec3
PICCOLI Michael <mpiccoli@emea.msad.sopra>
2016-10-14 17:05:22 +0100

    End of the day commit
    * 4 APIs completed, need testing and check the error handling

--- src/main/java/functionalities/EmployeeDAO.java
+++ src/main/java/functionalities/EmployeeDAO.java
@@ -2,9 +2,12 @@ package functionalities;
 
 import java.util.List;
 
+import javax.management.InvalidAttributeValueException;
+
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateOperations;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -25,63 +28,113 @@ import dataStructure.Objective;
  */
 public  class EmployeeDAO {
 
-	
+
 	public static List<Objective> getObjectivesForUser(int epmloyeeID){
 		Datastore datastore = getMongoDBConnection();
 		Query<Employee> query = datastore.createQuery(Employee.class).filter("employeeID =", epmloyeeID);
 		Employee e = query.get();
 		return e.getLatestVersionObjectives();
-		
+
 	}
 
-//
-//	public static void insertTempData(){
-//		try{
-//			Datastore datastore=getMongoDBConnection();
-//			Employee e1=new Employee(2312,8,"Red","Red","red.red@soprasteria.com","Junior Software Engineer"
-//					+ "",false,"Dhananjay Malpure",1111,"1993-09-14","2016-07-04",null,null,null);
-//			//Create a list of feedback
-//			Feedback fe1=new Feedback(1,2,"William","Wow, this guys is great at developing stuff","Internal","Email");
-//			Feedback fe2=new Feedback(2,3,"Red","Well, I was born ready!","Internal","Yammer");
-//			//Add the feedback to the employee object
-//			e1.addGenericFeedback(fe1);
-//			e1.addGenericFeedback(fe2);
-//			//Create 2 Objectives
-//			Objective o1=new Objective(1,0,1,"First Objective","This is the first objective that Michael has added to the system","2017-03");
-//			Objective o2=new Objective(2,1,2,"Second Objective","This is the second objective that Michael has added to the system","2016-12");
-//			//Add the objective to the employee object
-//			e1.addObjective(o1);
-//			e1.addObjective(o2);
-//
-//			//Add feedback to the objective
-//			Feedback fe3=new Feedback(1,2,"William","Wow, this guys is great at developing stuff","Internal","Email");
-//			Feedback fe4=new Feedback(2,3,"Red","Well, I was born ready!","Internal","Yammer");
-//			e1.addFeedbackToObjective(2, fe3);
-//			e1.addFeedbackToObjective(2, fe4);
-//			e1.addFeedbackToObjective(1, fe4);
-//
-//			datastore.save(e1);
-//		}
-//		catch(Exception e){
-//			System.out.println("There has been a problem!");
-//		}
-//	}
-//
-//	public static void getData(){
-//		try{
-//			Datastore datastore=getMongoDBConnection();
-//			Query<Employee> query = datastore.createQuery(Employee.class).filter("employeeID =", 2342);
-//			for(Employee e:query.asList()){
-//				System.out.println(e.toString());
-//				System.out.print("\n----------------------------------------------------------------\n");
-//			}
-//		}
-//		catch(Exception e){
-//			System.out.println("There has been a problem!");
-//		}
-//	}
+	public static List<Feedback> getFeedbackForUser(int employeeID){
 
-	
+		Datastore datastore = getMongoDBConnection();
+		Query<Employee> query = datastore.createQuery(Employee.class).filter("employeeID =", employeeID);
+		Employee e = query.get();
+		return e.getFeedbackList();
+	}//getFeedbackForUser
+
+
+	/**
+	 * 
+	 * @param employeeID
+	 * @return 
+	 * This method inserts a new objective for a specific employee given their ID
+	 * @throws InvalidAttributeValueException 
+	 */
+	public static boolean insertNewObjective(int employeeID, Object data) throws InvalidAttributeValueException, MongoException{
+		//Check the employeeID
+		if(employeeID>0){
+			if(data!=null && data instanceof Objective){
+				//Connect to the DB
+				Datastore conn=getMongoDBConnection();
+				//Retrieve Employee with the given ID
+				Query<Employee> querySearch = conn.createQuery(Employee.class).filter("employeeID =", employeeID);
+				Employee e = querySearch.get();
+				if(e!=null){
+					//Extract its List of Objectives
+					List<List<Objective>> dataFromDB=e.getObjectiveList();
+					//Add the new objective to the list
+					if(e.addObjective((Objective)data)){
+						//Update the List<objective> in the DB passing the new list
+						UpdateOperations<Employee> ops = conn.createUpdateOperations(Employee.class).set("objectives", dataFromDB);
+						//Commit the changes to the DB
+						conn.update(querySearch, ops);
+						return true;
+					}
+					else
+						throw new InvalidAttributeValueException("The given object couldn't be added to the objectives list");
+				}
+				else
+					throw new InvalidAttributeValueException("No employee found with the ID provided");
+			}
+			else
+				throw new InvalidAttributeValueException("The data provided is not valid");
+
+		}else
+			throw new InvalidAttributeValueException("The ID provided is not valid");
+	}
+
+	/*
+	public static void insertTempData(){
+		try{
+			Datastore datastore=getMongoDBConnection();
+			Employee e1=new Employee(3422,8,"Bill","Bill","bill.bill@soprasteria.com","Junior Software Engineer"
+					+ "",false,"Steven Malpure",3432,"1973-12-10","1987-11-19",null,null,null);
+			//Create a list of feedback
+			Feedback fe1=new Feedback(1,2,"William","Wow, this guys is great at developing stuff","Internal","Email");
+			Feedback fe2=new Feedback(2,3,"Red","Well, I was born ready!","Internal","Yammer");
+			//Add the feedback to the employee object
+			e1.addGenericFeedback(fe1);
+			e1.addGenericFeedback(fe2);
+			//Create 2 Objectives
+			Objective o1=new Objective(1,0,1,"First Objective","This is the first objective that Michael has added to the system","2017-03");
+			Objective o2=new Objective(2,1,2,"Second Objective","This is the second objective that Michael has added to the system","2016-12");
+			//Add the objective to the employee object
+			e1.addObjective(o1);
+			e1.addObjective(o2);
+
+			//Add feedback to the objective
+			Feedback fe3=new Feedback(1,2,"William","Wow, this guys is great at developing stuff","Internal","Email");
+			Feedback fe4=new Feedback(2,3,"Red","Well, I was born ready!","Internal","Yammer");
+			e1.addFeedbackToObjective(2, fe3);
+			e1.addFeedbackToObjective(2, fe4);
+			e1.addFeedbackToObjective(1, fe4);
+
+			datastore.save(e1);
+		}
+		catch(Exception e){
+			System.out.println("There has been a problem!");
+		}
+	}
+	 */
+	//
+	//	public static void getData(){
+	//		try{
+	//			Datastore datastore=getMongoDBConnection();
+	//			Query<Employee> query = datastore.createQuery(Employee.class).filter("employeeID =", 2342);
+	//			for(Employee e:query.asList()){
+	//				System.out.println(e.toString());
+	//				System.out.print("\n----------------------------------------------------------------\n");
+	//			}
+	//		}
+	//		catch(Exception e){
+	//			System.out.println("There has been a problem!");
+	//		}
+	//	}
+
+
 	private static Datastore getMongoDBConnection() throws MongoException{
 		String mongoClientURI = "mongodb://" + "michael" + ":" + "leahcim" + "@" + "172.25.111.64" + ":" + 27777 + "/" + "Development";
 		MongoClient client = new MongoClient(new MongoClientURI(mongoClientURI));
--- src/main/java/services/AppController.java
+++ src/main/java/services/AppController.java
@@ -1,14 +1,12 @@
 package services;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import dataStructure.Objective;
 import functionalities.EmployeeDAO;
 
@@ -25,18 +23,59 @@ import functionalities.EmployeeDAO;
 @RestController
 public class AppController {
 
-//	@RequestMapping(value="/", method=RequestMethod.GET)
-//	public void welcomePage(){
-//		//return ResponseEntity.ok("Welcome to the MyCareer Project");
-//		EmployeeDAO.insertTempData();
-//		EmployeeDAO.getData();
-//	}
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ResponseEntity<?> welcomePage(){
+		return ResponseEntity.ok("Welcome to the MyCareer Project");
+	}
 	
 	@RequestMapping(value="/getObjectives/{employeeID}", method=RequestMethod.GET)
-	public List<Objective> getObjectives(@PathVariable int employeeID){
-
-		return EmployeeDAO.getObjectivesForUser(employeeID);
+	public ResponseEntity<?> getObjectives(@PathVariable int employeeID){
+		if(employeeID>0)
+			return ResponseEntity.ok(EmployeeDAO.getObjectivesForUser(employeeID));
+		else
+			return ResponseEntity.badRequest().body("The given ID is invalid");
+	}
+	
+	@RequestMapping(value="/getFeedback/{employeeID}", method=RequestMethod.GET)
+	public ResponseEntity<?> getFeedback(@PathVariable int employeeID){
+		if(employeeID>0)
+			try{
+				error here,  not returning the actual error
+			return ResponseEntity.ok(EmployeeDAO.getFeedbackForUser(employeeID));
+			}catch(Exception e){
+				return ResponseEntity.badRequest().body(e);
+			}
+		else
+			return ResponseEntity.badRequest().body("The given ID is invalid");
 		
+	}
+	
+	/**
+	 * 
+	 * @param employeeID A value >0
+	 * @param title a string that doesn't exceed 150 characters
+	 * @param description a string that doesn't exceed 1000 characters
+	 * @param completedBy a valid month and year in the following format: yyyy-MM
+	 * @param progress a value between 0 and 2
+	 * @return
+	 */
+	@RequestMapping(value="/addObjective/{employeeID}", method=RequestMethod.POST)
+	public ResponseEntity<?> addObjectiveToAUser(
+			@PathVariable("employeeID") int employeeID,
+			@RequestParam(value="title") String title,
+			@RequestParam(value="description") String description,
+			@RequestParam(value="completedBy") String completedBy,
+			@RequestParam(value="progress") int progress){
+		//Validate the data
+		//boolean val=DataComingInValidator.validateAddObjective(employeeID,title,description,completedBy,progress);
+		//if(val){
+		try{
+			Objective obj=new Objective(progress,0,title,description,completedBy);
+			return ResponseEntity.ok(EmployeeDAO.insertNewObjective(employeeID,obj));
+		}
+		catch(Exception e){
+			return ResponseEntity.badRequest().body(e);
+		}
 	}
 	
 	
