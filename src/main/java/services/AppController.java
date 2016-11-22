c4f332c5e519014163e670f441e73dc1b0dfd11c
NACEF Ridhwan <ridhwan.nacef@soprasteria.com>
2016-11-22 15:47:45 +0000

    Added Progress attribute for development needs
    * Added in datastructure as well as rest controller for devneeds

--- src/main/java/services/AppController.java
+++ src/main/java/services/AppController.java
@@ -392,9 +392,10 @@ public class AppController {
 			@RequestParam(value="category") int cat,
 			@RequestParam(value="title") String title,
 			@RequestParam(value="description") String description,
-			@RequestParam(value="timeToCompleteBy") String timeToCompleteBy){
+			@RequestParam(value="timeToCompleteBy") String timeToCompleteBy,
+			@RequestParam int progress){
 		try{
-			DevelopmentNeed obj=new DevelopmentNeed(1,cat,title,description,timeToCompleteBy);
+			DevelopmentNeed obj=new DevelopmentNeed(1,progress,cat,title,description,timeToCompleteBy);
 			boolean inserted=EmployeeDAO.insertNewDevelopmentNeed(employeeID,obj);
 			if(inserted)
 				return ResponseEntity.ok("Development need inserted correctly!");
@@ -427,9 +428,10 @@ public class AppController {
 			@RequestParam(value="devNeedID") int devNeedID,
 			@RequestParam(value="title") String title,
 			@RequestParam(value="description") String description,
-			@RequestParam(value="timeToCompleteBy") String timeToCompleteBy){
+			@RequestParam(value="timeToCompleteBy") String timeToCompleteBy,
+			@RequestParam int progress){
 		try{
-			DevelopmentNeed obj=new DevelopmentNeed(devNeedID,cat,title,description,timeToCompleteBy);
+			DevelopmentNeed obj=new DevelopmentNeed(devNeedID,progress,cat,title,description,timeToCompleteBy);
 			boolean inserted=EmployeeDAO.addNewVersionDevelopmentNeed(employeeID, devNeedID, obj);
 			if(inserted)
 				return ResponseEntity.ok("Development need modified correctly!");
